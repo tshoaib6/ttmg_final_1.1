118665e3735c93b1bd45c0cb0e8bef3dc4d80b1a
Shoaib Tariq <43224905+tshoaib6@users.noreply.github.com>
2024-06-25 15:11:22 -0700

    fix

--- app/Controllers/OrdersContoller.php
+++ app/Controllers/OrdersContoller.php
@@ -67,7 +67,7 @@ class OrdersContoller extends BaseController
     public function ajax_sv_datatables_orders($id = "")
     {
         $db = db_connect();
-        $builder = $db->table('Look For Leads_orders')->select('agent, pkorderid as id ,lead_requested,remainingLeads,fkvendorstaffid,notes,status,pkorderid,fkclientid');
+        $builder = $db->table('ttmg_orders')->select('agent, pkorderid as id ,lead_requested,remainingLeads,fkvendorstaffid,notes,status,pkorderid,fkclientid');
 
         if ($id != 0) {
             $builder->where('fkvendorstaffid', $id);
@@ -119,7 +119,7 @@ class OrdersContoller extends BaseController
     public function ajax_Datatable_orders($id = "")
     {
         $db = db_connect();
-        $builder = $db->table('Look For Leads_orders')->select('agent, pkorderid as id ,lead_requested,remainingLeads,fkvendorstaffid,notes,status,pkorderid,fkclientid');
+        $builder = $db->table('ttmg_orders')->select('agent, pkorderid as id ,lead_requested,remainingLeads,fkvendorstaffid,notes,status,pkorderid,fkclientid');
 
         if ($id != 0) {
             $builder->where('categoryname', $id);
