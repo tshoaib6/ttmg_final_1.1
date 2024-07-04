78c2bc3e75bb16208794a02dfc39e81a52705d6d
shoaib <tshoaib10@gmail.com>
2024-07-04 08:38:49 +0500

    Update map_headers.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://crm.lookforleads.com/';
+    public string $baseURL = 'http://localhost:8888/ttmg_final/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
